c4f859d79489599f11f378dda7e7d26c0569a20b
dennishnf <dennishnf@gmail.com>
2025-07-04 04:00:20 +0200

    making website

--- update-mylaptop-windows.bat
+++ update-mylaptop-windows.bat
@@ -1,7 +1,7 @@
 @ECHO.
 git pull
 @ECHO.
-py "md2html.py" "C:\Users\Dennis\Desktop\dennishnf.github.io"
+py "md2html.py" "C:\Users\Dennis\Desktop\personalwebsite"
 @ECHO.
 git add -A
 git commit -m "making website"
